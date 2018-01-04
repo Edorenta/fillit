3a6ab44c1489a6f27f4a812b598fb5d23ebdcb1f
Paul DE RENTY <pde-rent@e3r8p1.42.fr>
2018-01-04 18:34:57 +0100

    final fillit

--- src_txt_to_tab.c
+++ src_txt_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: pde-rent <pde-rent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/04 14:19:12 by pde-rent          #+#    #+#             */
-/*   Updated: 2018/01/04 16:42:20 by pde-rent         ###   ########.fr       */
+/*   Updated: 2018/01/04 18:34:47 by pde-rent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static	unsigned __int128	ft_pow(int x, unsigned int y)
 }
 
 unsigned __int128			*txt_to_tab(char *filename, unsigned __int128 *tab, int grid_size,
-		int nb_blocks)
+							int nb_blocks)
 {
 	int		cnt[3];
 	int		fd;
